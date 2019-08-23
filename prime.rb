8a2d2ef9520e3a9c7706f809d81e250bb6f0a297
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 02:34:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -35,3 +35,7 @@
 #   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
 #   true
 # end
+def prime?(num)
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+end
