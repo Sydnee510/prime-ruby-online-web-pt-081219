c760df80cbf6e9cb6b1c01e8e914ff8cbf314970
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 02:27:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -30,18 +30,3 @@
 
 #   true
 # end
-def prime?(array)
-List<Integer> odd = new ArrayList<Integer>()
-List<Integer> even = new ArrayList<Integer>()
-for (int i : array_sort) {
-    if ((i & 1) == 1) {
-        odd.add(i)
-    } else {
-        even.add(i)
-    }
-}
-Collections.sort(odd)
-Collections.sort(even)
-# System.out.println("Odd:" + odd)
-# System.out.println("Even:" + even)
-end
