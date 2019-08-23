7f90b136f655897725afeec5255501202a95667c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 01:57:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
 def prime?(n)
   # def is_prime n
+  i = 0
   odd_values = []
-  for d in 2..(n - 1)
-   if (n % d) == 0
+  for i in 2..(n - 1)
+   if (n % i) == 0
     return false
    end
   end
