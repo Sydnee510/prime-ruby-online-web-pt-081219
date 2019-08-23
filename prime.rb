4688b02b8ff0a178051d542171e8a56ad4c5e471
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 02:38:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,41 +1,10 @@
-# # Add  code here!
-# def prime?(n)
-#   # def is_prime n
-#   i = 1
-#   odd_values = []
-#   for i in 2..(n - 1)
-#   odd_values << yield(n[i])
-#     i = i + 1
-#   end 
-#   if (n % i) == 0
-#     return false
-#     false
-#   else 
-#     true
-#   end
-# # if n.include?(false)
-# #   false
-# #   else
-# #   true
-# # end
-# end
-# def prime? n
-#   for d in 2..(n - 1)
-#   if (n % d) == 0
-#     return false
-#   else
-#     true
-#   end
-#   end
 
-#   true
-# end
+ def prime?(num)
+   return false if num <= 1
+   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+   true
+ end
 # def prime?(num)
 #   return false if num <= 1
-#   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-#   true
+#   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 # end
-def prime?(num)
-  return false if num <= 1
-  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
-end
