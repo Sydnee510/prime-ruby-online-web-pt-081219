0472a64b68882a48a954b5c9219f08bd6d8aa427
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 02:21:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -30,3 +30,17 @@
 
 #   true
 # end
+
+List<Integer> odd = new ArrayList<Integer>();
+List<Integer> even = new ArrayList<Integer>();
+for (int i : array_sort) {
+    if ((i & 1) == 1) {
+        odd.add(i);
+    } else {
+        even.add(i);
+    }
+}
+Collections.sort(odd);
+Collections.sort(even);
+System.out.println("Odd:" + odd);
+System.out.println("Even:" + even);
