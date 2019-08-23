be77280ddb8a6e4a5d854598f4f893ec6d3609dc
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 02:19:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,30 @@
-# Add  code here!
-def prime?(n)
-  # def is_prime n
-  i = 1
-  odd_values = []
-  for i in 2..(n - 1)
-   odd_values << yield(n[i])
-    i = i + 1
-  end 
-   if (n % i) == 0
-    return false
-    false
-  else 
-    true
-   end
-# if n.include?(false)
-#   false
-#   else
-#   true
+# # Add  code here!
+# def prime?(n)
+#   # def is_prime n
+#   i = 1
+#   odd_values = []
+#   for i in 2..(n - 1)
+#   odd_values << yield(n[i])
+#     i = i + 1
+#   end 
+#   if (n % i) == 0
+#     return false
+#     false
+#   else 
+#     true
+#   end
+# # if n.include?(false)
+# #   false
+# #   else
+# #   true
+# # end
 # end
-end
+ def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
+
+  true
+ end
